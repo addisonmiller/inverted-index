14b28cedca20537bbac1f22898635c3979195b7d
tom.gidden <tom.gidden@223c0c60-904f-0410-a7a8-891d9b0ad5c3>
2008-06-14 01:03:07 +0000

    Separated article table (definitely a "test") from the core search tables (definitely "sql")

---
+++ sql/tables.sql
@@ -0,0 +1,46 @@
+/*
+ * Tables
+ * Inverted Index Toolkit  <http://code.google.com/p/inverted-index/>
+ * Apache License 2.0, blah blah blah.
+ */
+
+
+DROP TABLE IF EXISTS search_index;
+DROP TABLE IF EXISTS search_class;
+DROP TABLE IF EXISTS word;
+
+/*
+ * This table contains a list of words as used in the search index.
+ */
+CREATE TABLE word (
+  word_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  string VARCHAR(255) NOT NULL,
+  PRIMARY KEY  (word_id),
+  UNIQUE KEY ix_wordstr (string)
+) ENGINE=InnoDB;
+
+/*
+ * This table contains a list of search classes.
+ */
+CREATE TABLE search_class (
+  search_class_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  source_table VARCHAR(255) NOT NULL,
+  source_id VARCHAR(255) NOT NULL,
+  PRIMARY KEY (search_class_id),
+  KEY (name)
+) ENGINE=InnoDB;
+
+/*
+ * This table contains the search index
+ */
+CREATE TABLE search_index (
+  id INT(10) UNSIGNED NOT NULL,
+  search_class_id INT(11) UNSIGNED NOT NULL,
+  position INT(10) UNSIGNED NOT NULL,
+  word_id INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY  (id,search_class_id,position),
+  KEY ix_wfp (word_id,search_class_id,position),
+  FOREIGN KEY (word_id) REFERENCES word (word_id),
+  FOREIGN KEY (search_class_id) REFERENCES search_class (search_class_id)
+) ENGINE=InnoDB;
--- tests/create_tables.sql
+++ tests/create_tables.sql
@@ -5,9 +5,6 @@
  */
 
 
-DROP TABLE IF EXISTS search_index;
-DROP TABLE IF EXISTS search_class;
-DROP TABLE IF EXISTS word;
 DROP TABLE IF EXISTS article;
 
 /*
@@ -21,38 +18,3 @@ CREATE TABLE IF NOT EXISTS article (
   PRIMARY KEY  (article_id)
 ) ENGINE=InnoDB;
 
-/*
- * This table contains a list of words as used in the search index.
- */
-CREATE TABLE word (
-  word_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  string VARCHAR(255) NOT NULL,
-  PRIMARY KEY  (word_id),
-  UNIQUE KEY ix_wordstr (string)
-) ENGINE=InnoDB;
-
-/*
- * This table contains a list of search classes.
- */
-CREATE TABLE search_class (
-  search_class_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  source_table VARCHAR(255) NOT NULL,
-  source_columns VARCHAR(255) NOT NULL,
-  PRIMARY KEY (search_class_id),
-  KEY (name)
-) ENGINE=InnoDB;
-
-/*
- * This table contains the search index
- */
-CREATE TABLE search_index (
-  id INT(10) UNSIGNED NOT NULL,
-  search_class_id INT(11) UNSIGNED NOT NULL,
-  position INT(10) UNSIGNED NOT NULL,
-  word_id INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY  (id,search_class_id,position),
-  KEY ix_wfp (word_id,search_class_id,position),
-  FOREIGN KEY (word_id) REFERENCES word (word_id),
-  FOREIGN KEY (search_class_id) REFERENCES search_class (search_class_id)
-) ENGINE=InnoDB;
