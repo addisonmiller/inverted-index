7fde8eef1d9d836fd5f24d77fe432198423ceaf3
tom.gidden <tom.gidden@223c0c60-904f-0410-a7a8-891d9b0ad5c3>
2008-06-21 21:21:25 +0000

    * Added "guten" project, which contains a sample corpus of text for a more realistic (while still quite modest) database.
    * Moved stuff about a bit
    
    * Played with indexing

--- trunk/README.sql
+++ trunk/README.sql
@@ -18,12 +18,17 @@
 \. sql/sp_indexString.sql
 \. sql/sp_searchBasic.sql
 \. sql/sp_searchPhrase.sql
-\. tests/create_tables.sql
+
+-- -----------------------------------------------------------------------
+-- and the sample data, which shows how to set up the correct triggers
+-- -----------------------------------------------------------------------
+
+\. tests/setup.sql
 \. tests/triggers.sql
 \. tests/data.sql
 
 -- -----------------------------------------------------------------------
--- then try out the searching procedures:
+-- Then try out the searching procedures:
 -- -----------------------------------------------------------------------
 
 CALL searchBasic(classID("article._all"), "the air");
@@ -49,20 +54,38 @@ CALL searchPhrase(classID("article.body"), "best of times");
 
 SELECT @_sql;
 
+
 -- -----------------------------------------------------------------------
+-- You can also try some of the bigger example files in the 'guten' branch
+-- (in SVN).  This is a much larger dataset sourced from Project Gutenberg
+-- that takes longer to install, but gives a good test of the search
+-- functions.
+--
+--
 -- Notes:
--- 
+--
 -- Column indexing these tables is critical.  It's worth taking a good
 -- look at this with real data.  The sample data here will not demonstrate
--- correct indexing.  Use DESCRIBE/EXPLAIN to view the query plan.
--- 
+-- correct indexing.
+--
+-- Try out a few of the "CREATE INDEX search_index" lines in
+-- sql/tables.sql, and see if they improve performance: Do a sample query
+-- (using CALL searchBasic(...)), then SELECT @_sql, then DESCRIBE the
+-- resulting query to see if the new index was used (by looking in the
+-- 'key' column)
+--
+-- Extra indexes can significantly slow down indexing time, but if they
+-- are used, they may massively improve query performance.  The best thing
+-- to do is try them out!
+--
+--
 -- This implementation currently processes words in a very simple way: it
 -- just trims whitespace from the start and end of the word, and it
 -- converts it to lowercase.
--- 
+--
 -- Words are considered to follow the pattern: [[:alnum:]]+, ie. a string
 -- of alphanumeric characters.
--- 
+--
 -- Different applications of this code will need different rules.
 -- Addresses, in particular, have some specific needs, such as converting
 -- ordinals (1st <=> First); adding alternatives (Basement Flat <=> Garden
--- trunk/sql/tables.sql
+++ trunk/sql/tables.sql
@@ -15,9 +15,9 @@ DROP TABLE IF EXISTS word;
 CREATE TABLE word (
   word_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   string VARCHAR(255) NOT NULL,
-  PRIMARY KEY  (word_id),
-  UNIQUE KEY ix_wordstr (string)
-) ENGINE=InnoDB;
+  PRIMARY KEY (word_id),
+  UNIQUE KEY (string)
+) ENGINE=MyISAM;
 
 /*
  * This table contains a list of search classes.
@@ -27,18 +27,40 @@ CREATE TABLE search_class (
   name VARCHAR(255) NOT NULL,
   PRIMARY KEY (search_class_id),
   KEY (name)
-) ENGINE=InnoDB;
+) ENGINE=MyISAM;
 
 /*
- * This table contains the search index
+ * This table contains the search index.  It's difficult to guess what
+ * indexes to put on this table, but it's clear that good indexing is
+ * vital for this table in particular.
  */
 CREATE TABLE search_index (
   id INT(10) UNSIGNED NOT NULL,
   search_class_id INT(11) UNSIGNED NOT NULL,
   position INT(10) UNSIGNED NOT NULL,
   word_id INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY  (id,search_class_id,position),
-  KEY ix_wfp (word_id,search_class_id,position),
+
+  PRIMARY KEY (id,search_class_id,position),
   FOREIGN KEY (word_id) REFERENCES word (word_id),
-  FOREIGN KEY (search_class_id) REFERENCES search_class (search_class_id)
-) ENGINE=InnoDB;
+  FOREIGN KEY (search_class_id) REFERENCES search_class (search_class_id),
+) ENGINE=MyISAM;
+
+/*
+ * More indexes slows down index creation (and update) time significantly,
+ * but _can_ speed up query time even more significantly.  Use "SELECT
+ * @_sql" and then DESCRIBE the resulting query to work out what indexes
+ * are being used, and drop the others.
+ *
+ * The "wcp" index appears to be the most important, but the others are
+ * worth evaluating.
+ */
+
+CREATE INDEX wcp ON search_index (word_id, search_class_id, position);
+-- CREATE INDEX cwp ON search_index (search_class_id, word_id, position);
+-- CREATE INDEX cpw ON search_index (search_class_id, position, word_id);
+-- CREATE INDEX cip ON search_index (search_class_id, id, position);
+-- CREATE INDEX cpi ON search_index (search_class_id, position, id);
+-- CREATE INDEX iwc ON search_index (id, word_id, search_class_id);
+-- CREATE INDEX icw ON search_index (id, search_class_id, word_id);
+-- CREATE INDEX pci ON search_index (position, search_class_id, id);
+-- CREATE INDEX pic ON search_index (position, id, search_class_id);
