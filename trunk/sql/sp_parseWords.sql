506359c6a29ccbea9f620207e65e5c870ef9dcab
tom.gidden <tom.gidden@223c0c60-904f-0410-a7a8-891d9b0ad5c3>
2008-06-22 11:44:46 +0000

    * Renamed "guten" to "corpus", to detach from the source
    * Refactored the search code, to use a main "search" procedure and a separate query builder.  This allows construction of more complex queries.
    
    * Removed "parseWords" from query builder in favour of "nextWord".  While parseWords may be more efficient and more flexible (not sure), nextWord doesn't affect the database, allowing "READS SQL DATA" rather than "MODIFIES SQL DATA".  This might improve concurrency and caching.  As this was a late change, I've included a working implementation of buildSearchParts that uses parseWords.  It might be worth benchmarking them against each other.
    
    * Rewrote some of the examples.
    
    * Removed ._all from "main" search classes.  Ugly.

--- trunk/sql/sp_parseWords.sql
+++ trunk/sql/sp_parseWords.sql
@@ -5,11 +5,11 @@ DELIMITER ;;
 CREATE PROCEDURE parseWords (in_string TEXT) MODIFIES SQL DATA
 thisproc:BEGIN
 
-DECLARE pos           INT DEFAULT 0;
+DECLARE pos			  INT DEFAULT 0;
 DECLARE string_length INT DEFAULT length(in_string);
-DECLARE capture       INT DEFAULT NULL;
-DECLARE buf           VARCHAR(128) DEFAULT NULL; -- word buffer
-DECLARE chr           VARCHAR(1);
+DECLARE capture		  INT DEFAULT NULL;
+DECLARE buf			  VARCHAR(128) DEFAULT NULL; -- word buffer
+DECLARE chr			  VARCHAR(1);
 
 
 -- Creates temporary table for storing and the retrieval of
@@ -18,9 +18,9 @@ DECLARE chr           VARCHAR(1);
 
 DROP TEMPORARY TABLE IF EXISTS parseWords_result; -- (potentially) dangerous.
 CREATE TEMPORARY TABLE parseWords_result(
-       pos int primary key auto_increment,  -- auto inc is used here for automatic
-					    -- word position enumeration
-       word varchar(128)
+	   pos int primary key auto_increment,	-- auto inc is used here for automatic
+						-- word position enumeration
+	   word varchar(128)
 );
 
 -- Iterates over each character in the in_string
@@ -28,7 +28,7 @@ REPEAT
 	SET chr =  substr(in_string, pos,1);
 	IF chr is NULL THEN
 	   LEAVE thisproc;
-	END IF;	
+	END IF;
 
 	SET capture = IF(chr REGEXP '^[a-zA-Z0-9]$', 1, NULL);
 
@@ -40,7 +40,7 @@ REPEAT
 	IF ((capture IS NULL OR pos = string_length) AND buf IS NOT NULL) THEN
 	   INSERT INTO parseWords_result (word) VALUES (buf);
 	   SET buf = NULL;
-	END IF;   
+	END IF;
 
 	SET pos=pos+1;
 UNTIL pos > string_length
--- trunk/sql/sp_searchBasic.sql
+++ trunk/sql/sp_searchBasic.sql
@@ -6,6 +6,23 @@
  * This constructs and executes a simple "AND" query, based on an input
  * string of words.  Rather than including the words themselves in the
  * query, it looks them up in advance.
+ *
+ * Parameters:
+ *
+ * _classId:   the class ID to be searched.  eg. classID('doc')
+ *
+ * _sentence:  the sentence to be searched.  eg. "best of times"
+ *
+ * Optional control variables:
+ *
+ *   SET @_noExecute = TRUE;
+ *      -- Returns the query parts, rather than executing it, to assist
+ *      -- building of custom queries.
+ *
+ *   SET @_suppressGlobals = TRUE;
+ *      -- Do not set the global variable parts, and preserve @_sql if
+ *      -- possible.
+ *
  */
 
 DROP PROCEDURE IF EXISTS searchBasic;
@@ -14,82 +31,7 @@ DELIMITER ;;
 CREATE PROCEDURE `searchBasic`(_classId INT UNSIGNED, _sentence TEXT)
 thisproc:BEGIN
 
---
--- Internal Variables
---
-
-DECLARE _word VARCHAR(255) DEFAULT NULL;
-DECLARE _wordCount INT UNSIGNED DEFAULT '0';
-DECLARE _wordId INT UNSIGNED;
-
-DECLARE _wheres TEXT DEFAULT '';
-
--- Iterator counter for word loop
-DECLARE _i INT DEFAULT 0;
-
--- Defining cursor for iterating over parsed input words
-DECLARE _continue_loop_words INT DEFAULT 1;
-DECLARE cursor_words CURSOR FOR SELECT word FROM parseWords_result;
-DECLARE CONTINUE HANDLER FOR NOT FOUND SET _continue_loop_words = NULL;
-
--- Call proc to parse input words into temp table.
-CALL parseWords(_sentence);
-SELECT count(*) FROM parseWords_result INTO _wordCount;
-
--- Leave procedure if there are no parsed results.
-IF !(_wordCount > 0) THEN
-   LEAVE thisproc;
-END IF;
-
--- Start the query with the data table itself.
-SET @_sql = CONCAT('SELECT i0.id, i0.position FROM ');
-
-OPEN cursor_words;
-loop_words: LOOP
-    -- Cursor control (the table contains word positions, but its
-    -- not fetched)
-    FETCH cursor_words INTO _word;
-    IF (_continue_loop_words is NULL) THEN
-       LEAVE loop_words;
-    END IF;
-
-
-    -- Get the ID for this word
-    SET _wordId = wordID(_word, FALSE);
-
-    -- If no word was found, this whole thing's a waste of time
-    -- anyway, as no rows will be returned.
-    IF (_wordId IS NULL) THEN
-        LEAVE thisproc;
-    END IF;
-
-    -- Assemble an inner join from one copy of the index table to the
-    -- previous one.
-    IF (_i>0) THEN
-        SET @_sql = CONCAT(@_sql, ' INNER JOIN search_index AS i',_i,
-                            ' ON i',(_i-1),'.id = i',_i,'.id ');
-        SET _wheres = CONCAT(_wheres,
-                            ' AND i',_i,'.search_class_id = ',_classId,
-                            ' AND i',_i,'.word_id = ',_wordId);
-    ELSE
-        SET @_sql = CONCAT(@_sql, ' search_index AS i0 ');
-        SET _wheres = CONCAT(' i0.word_id = ',_wordId,
-                             ' AND i0.search_class_id = ',_classId);
-    END IF;
-
-    SET _i=_i+1;
-END LOOP loop_words;
-
-
--- Construct query SQL, and execution
-SET @_sql = CONCAT(@_sql, ' WHERE ', _wheres, ' GROUP BY i0.id');
-PREPARE query FROM @_sql;
-EXECUTE query;
-
--- Clear up
-CLOSE cursor_words;
-DROP PREPARE query;
-
+CALL search(_classId, _sentence, 'and', NULL, NULL, NULL);
 
 END;;
 DELIMITER ;
